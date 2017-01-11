eebfb5c20951ca7af21de144d4a2d611d0c5f6a1
DaTechNinja <david.techninja@gmail.com>
2017-01-11 14:34:34 -0500

    Removed debug console message

--- bmo-investorline-infinite-session.user.js
+++ bmo-investorline-infinite-session.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://openuserjs.org/libs/DaTechNinja/bmo-investorline-infinite-session
 // @description  Disables 15 minute session inactivity logout on BMO InvestorLine. Stay logged in as long as you want!
 // @author       DaTechNinja
-// @version      1.0.3
+// @version      1.0.4
 // @encoding     utf-8
 // @license      https://raw.githubusercontent.com/DaTechNinja/bmo-investorline-infinite-session/master/LICENSE
 // @icon         https://raw.githubusercontent.com/DaTechNinja/bmo-investorline-infinite-session/master/favicon.ico
@@ -32,7 +32,6 @@
 
     $(document).ready(function() {
         function getRandomInt(min, max) {
-            console.log("Generating random number");
             return Math.floor(Math.random() * (max - min + 1)) + min;
         }
 
